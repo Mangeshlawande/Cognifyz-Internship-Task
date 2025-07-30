1f1dd6f64dec12e5fc4417392bbea6e2c08086a1
Mangeshlawande <mangeshlawande223@gmail.com>
2025-07-30 20:31:09 +0530

    add task1 to the code base.

---
+++ task1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/*
+Task: String Reversal
+
+Description:Write a program that takes a string as input and reverses it. Display the reversed string on the console.
+*/
+void reverseString(char s[])
+{
+    int j, i;
+    char t;
+    for (j = 0; s[j] != '\0'; j++)
+    {
+    }
+    j = j - 1;
+
+    for (i = 0; i < j; i++, j--)
+    {
+        t = s[i];
+        s[i] = s[j];
+        s[j] = t;
+    }
+    cout << s << endl;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of string : \n");
+    scanf("%d", &n);
+    char name[n];
+    printf("Enter string : \n");
+    scanf("%s", name);
+
+    printf("Reverse of string : ");
+    reverseString(name);
+
+    return 0;
+};
